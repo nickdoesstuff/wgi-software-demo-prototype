1d2f3eb114d155a1cf9219adc88328eedda69f4a
Nicholas A Shelton <nickshelton1376@gmail.com>
2022-12-26 09:48:44 -0500

    Initial Prototype

---
+++ story_content/user.js
@@ -0,0 +1,385 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5lAy5StBkMt":
+        Script1();
+        break;
+      case "67CrG5zgBRq":
+        Script2();
+        break;
+      case "6EXFWcFcxUH":
+        Script3();
+        break;
+      case "6f2QgLEuwqt":
+        Script4();
+        break;
+      case "6foKM2iQ8P6":
+        Script5();
+        break;
+      case "6f78MAmGynH":
+        Script6();
+        break;
+      case "6nUJXlF7JqO":
+        Script7();
+        break;
+      case "5XKHCp4drXz":
+        Script8();
+        break;
+      case "6GEub4DTtgH":
+        Script9();
+        break;
+      case "5bvmEPZnKcg":
+        Script10();
+        break;
+      case "6HMNM3WAYRa":
+        Script11();
+        break;
+      case "6H5D7hCTYwx":
+        Script12();
+        break;
+      case "6JCfW7UBLvT":
+        Script13();
+        break;
+      case "5daIqmxXJgM":
+        Script14();
+        break;
+      case "5mzpMI2bvl8":
+        Script15();
+        break;
+      case "6Nikb9Y0CMK":
+        Script16();
+        break;
+      case "6mLncBZ1uG8":
+        Script17();
+        break;
+      case "6JI8AGyfUM9":
+        Script18();
+        break;
+      case "6bEM9mvq6VT":
+        Script19();
+        break;
+      case "5enSllc0nF3":
+        Script20();
+        break;
+      case "65EfKavFgbH":
+        Script21();
+        break;
+  }
+}
+
+function Script1()
+{
+  var dummy = document.createElement('input'),
+    text = window.location.href;
+
+document.body.appendChild(dummy);
+dummy.value = text;
+dummy.select();
+document.execCommand('copy');
+document.body.removeChild(dummy);
+
+var player = GetPlayer();
+player.SetVar('Course_URL', text);
+}
+
+function Script2()
+{
+  var dummy = document.createElement('input'),
+    text = window.location.href;
+
+document.body.appendChild(dummy);
+dummy.value = text;
+dummy.select();
+document.execCommand('copy');
+document.body.removeChild(dummy);
+
+var player = GetPlayer();
+player.SetVar('Course_URL', text);
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+var Course_Title = player.GetVar("Course_Title");
+var Course_URL = player.GetVar("Course_URL");
+
+emailTo = "";
+emailCC = "";
+emailSub = "Check out the " + Course_Title + " resource!";
+emailBody = "There's some really handy information in the " + Course_Title + " resource.%0d%0a%0d%0aHere's the link:%0d%0a" + Course_URL + "%0d%0a%0d%0a";
+
+
+location.href = "mailto:"+emailTo+'?cc='+emailCC+'&subject='+emailSub+'&body='+emailBody;
+}
+
+function Script4()
+{
+  var dummy = document.createElement('input'),
+    text = window.location.href;
+
+document.body.appendChild(dummy);
+dummy.value = text;
+dummy.select();
+document.execCommand('copy');
+document.body.removeChild(dummy);
+
+var player = GetPlayer();
+player.SetVar('Course_URL', text);
+}
+
+function Script5()
+{
+  var dummy = document.createElement('input'),
+    text = window.location.href;
+
+document.body.appendChild(dummy);
+dummy.value = text;
+dummy.select();
+document.execCommand('copy');
+document.body.removeChild(dummy);
+
+var player = GetPlayer();
+player.SetVar('Course_URL', text);
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+var Course_Title = player.GetVar("Course_Title");
+var Course_URL = player.GetVar("Course_URL");
+
+emailTo = "";
+emailCC = "";
+emailSub = "Check out the " + Course_Title + " resource!";
+emailBody = "There's some really handy information in the " + Course_Title + " resource.%0d%0a%0d%0aHere's the link:%0d%0a" + Course_URL + "%0d%0a%0d%0a";
+
+
+location.href = "mailto:"+emailTo+'?cc='+emailCC+'&subject='+emailSub+'&body='+emailBody;
+}
+
+function Script7()
+{
+  var dummy = document.createElement('input'),
+    text = window.location.href;
+
+document.body.appendChild(dummy);
+dummy.value = text;
+dummy.select();
+document.execCommand('copy');
+document.body.removeChild(dummy);
+
+var player = GetPlayer();
+player.SetVar('Course_URL', text);
+}
+
+function Script8()
+{
+  var dummy = document.createElement('input'),
+    text = window.location.href;
+
+document.body.appendChild(dummy);
+dummy.value = text;
+dummy.select();
+document.execCommand('copy');
+document.body.removeChild(dummy);
+
+var player = GetPlayer();
+player.SetVar('Course_URL', text);
+}
+
+function Script9()
+{
+  var player = GetPlayer();
+var Course_Title = player.GetVar("Course_Title");
+var Course_URL = player.GetVar("Course_URL");
+
+emailTo = "";
+emailCC = "";
+emailSub = "Check out the " + Course_Title + " resource!";
+emailBody = "There's some really handy information in the " + Course_Title + " resource.%0d%0a%0d%0aHere's the link:%0d%0a" + Course_URL + "%0d%0a%0d%0a";
+
+
+location.href = "mailto:"+emailTo+'?cc='+emailCC+'&subject='+emailSub+'&body='+emailBody;
+}
+
+function Script10()
+{
+  var dummy = document.createElement('input'),
+    text = window.location.href;
+
+document.body.appendChild(dummy);
+dummy.value = text;
+dummy.select();
+document.execCommand('copy');
+document.body.removeChild(dummy);
+
+var player = GetPlayer();
+player.SetVar('Course_URL', text);
+}
+
+function Script11()
+{
+  var dummy = document.createElement('input'),
+    text = window.location.href;
+
+document.body.appendChild(dummy);
+dummy.value = text;
+dummy.select();
+document.execCommand('copy');
+document.body.removeChild(dummy);
+
+var player = GetPlayer();
+player.SetVar('Course_URL', text);
+}
+
+function Script12()
+{
+  var player = GetPlayer();
+var Course_Title = player.GetVar("Course_Title");
+var Course_URL = player.GetVar("Course_URL");
+
+emailTo = "";
+emailCC = "";
+emailSub = "Check out the " + Course_Title + " resource!";
+emailBody = "There's some really handy information in the " + Course_Title + " resource.%0d%0a%0d%0aHere's the link:%0d%0a" + Course_URL + "%0d%0a%0d%0a";
+
+
+location.href = "mailto:"+emailTo+'?cc='+emailCC+'&subject='+emailSub+'&body='+emailBody;
+}
+
+function Script13()
+{
+  var dummy = document.createElement('input'),
+    text = window.location.href;
+
+document.body.appendChild(dummy);
+dummy.value = text;
+dummy.select();
+document.execCommand('copy');
+document.body.removeChild(dummy);
+
+var player = GetPlayer();
+player.SetVar('Course_URL', text);
+}
+
+function Script14()
+{
+  var dummy = document.createElement('input'),
+    text = window.location.href;
+
+document.body.appendChild(dummy);
+dummy.value = text;
+dummy.select();
+document.execCommand('copy');
+document.body.removeChild(dummy);
+
+var player = GetPlayer();
+player.SetVar('Course_URL', text);
+}
+
+function Script15()
+{
+  var player = GetPlayer();
+var Course_Title = player.GetVar("Course_Title");
+var Course_URL = player.GetVar("Course_URL");
+
+emailTo = "";
+emailCC = "";
+emailSub = "Check out the " + Course_Title + " resource!";
+emailBody = "There's some really handy information in the " + Course_Title + " resource.%0d%0a%0d%0aHere's the link:%0d%0a" + Course_URL + "%0d%0a%0d%0a";
+
+
+location.href = "mailto:"+emailTo+'?cc='+emailCC+'&subject='+emailSub+'&body='+emailBody;
+}
+
+function Script16()
+{
+  var dummy = document.createElement('input'),
+    text = window.location.href;
+
+document.body.appendChild(dummy);
+dummy.value = text;
+dummy.select();
+document.execCommand('copy');
+document.body.removeChild(dummy);
+
+var player = GetPlayer();
+player.SetVar('Course_URL', text);
+}
+
+function Script17()
+{
+  var dummy = document.createElement('input'),
+    text = window.location.href;
+
+document.body.appendChild(dummy);
+dummy.value = text;
+dummy.select();
+document.execCommand('copy');
+document.body.removeChild(dummy);
+
+var player = GetPlayer();
+player.SetVar('Course_URL', text);
+}
+
+function Script18()
+{
+  var player = GetPlayer();
+var Course_Title = player.GetVar("Course_Title");
+var Course_URL = player.GetVar("Course_URL");
+
+emailTo = "";
+emailCC = "";
+emailSub = "Check out the " + Course_Title + " resource!";
+emailBody = "There's some really handy information in the " + Course_Title + " resource.%0d%0a%0d%0aHere's the link:%0d%0a" + Course_URL + "%0d%0a%0d%0a";
+
+
+location.href = "mailto:"+emailTo+'?cc='+emailCC+'&subject='+emailSub+'&body='+emailBody;
+}
+
+function Script19()
+{
+  var dummy = document.createElement('input'),
+    text = window.location.href;
+
+document.body.appendChild(dummy);
+dummy.value = text;
+dummy.select();
+document.execCommand('copy');
+document.body.removeChild(dummy);
+
+var player = GetPlayer();
+player.SetVar('Course_URL', text);
+}
+
+function Script20()
+{
+  var dummy = document.createElement('input'),
+    text = window.location.href;
+
+document.body.appendChild(dummy);
+dummy.value = text;
+dummy.select();
+document.execCommand('copy');
+document.body.removeChild(dummy);
+
+var player = GetPlayer();
+player.SetVar('Course_URL', text);
+}
+
+function Script21()
+{
+  var player = GetPlayer();
+var Course_Title = player.GetVar("Course_Title");
+var Course_URL = player.GetVar("Course_URL");
+
+emailTo = "";
+emailCC = "";
+emailSub = "Check out the " + Course_Title + " resource!";
+emailBody = "There's some really handy information in the " + Course_Title + " resource.%0d%0a%0d%0aHere's the link:%0d%0a" + Course_URL + "%0d%0a%0d%0a";
+
+
+location.href = "mailto:"+emailTo+'?cc='+emailCC+'&subject='+emailSub+'&body='+emailBody;
+}
+
